32dc50eb2d76d9b81f20aa04163e5bd857269380
davidcm <david000anthony@gmail.com>
2024-01-12 18:43:59 -0500

    Add accepted origin for RESTAPI-nodejs-mysql

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -4,7 +4,8 @@ const ACCEPTED_ORIGNIS = [
   'http://localhost:8080',
   'http://localhost:3000',
   'http://localhost:19006',
-  'http://192.168.1.46:8081'
+  'http://192.168.1.46:8081',
+  'https://deybitcm.github.io/RESTAPI-nodejs-mysql'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGNIS } = {}) => cors({
