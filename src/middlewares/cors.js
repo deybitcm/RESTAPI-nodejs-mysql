65cb9975b7f88b5af98fc476a1e654ac6fe1cb86
davidcm <david000anthony@gmail.com>
2024-01-12 23:58:41 -0500

    Add start script and update cors middleware

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,12 +1,6 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGNIS = [
-  'http://localhost:8080',
-  'http://localhost:3000',
-  'http://localhost:19006',
-  'http://192.168.1.46:8081',
-  'https://deybitcm.github.io'
-]
+const ACCEPTED_ORIGNIS = []
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGNIS } = {}) => cors({
   origin: (origin, callback) => {
